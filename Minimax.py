1dc31563587977fcf1e95f1ee34eb2b46599ac9f
Katherine <ocheretianfws@gmail.com>
2021-03-21 02:43:23 +0200

    6x6

--- Minimax.py
+++ Minimax.py
@@ -1,72 +1,97 @@
-def minimax(board, max_min, AI_letter, depth):
-    if depth==0:
-        someone_won = board.get_win()
-        if someone_won is None:
-            else_won = counting(board)
-            if else_won == AI_letter:
-                return 1
-            elif else_won != AI_letter and else_won !=0:
-                return -1
-            else:
-                return 0
+import random
 
-    someone_won = board.get_win()
+
+def minimax(checkers, max_min, AI_letter, depth):
+    someone_won = checkers.get_win()
     if someone_won == AI_letter:
-        return 1
+        return 13
     elif someone_won != AI_letter and someone_won is not None:
-        return -1
+        return -13
 
+    if depth == 0:
+        a = 0
+        r = 0
+        for row in range(len(checkers.board)):
+            for kolm in range(len(checkers.board[row])):
+                if checkers.board[row][kolm].lower() == 'a':
+                    a = a+1
+                elif checkers.board[row][kolm].lower() == 'r':
+                    r = r+1
+
+        if a>r:
+            if  AI_letter=='a':
+                return a + (6-r)
+            else:
+                return -r + (12-a)
+        elif r>a:
+            if AI_letter == 'a':
+                return r + (6 - a)
+            else:
+                return -a + (12 - r)
+        else:
+            return 0
 
 
     scores = []
-    possible_moves, order = board.get_possible_moves()
+    possible_moves = checkers.get_possible_moves()
     score = 0
     for move in possible_moves:
-        board.make_move(move)
-        # board.print()
-        if board.whose_turn() == AI_letter:
-            score = minimax(board, 1, AI_letter, depth-1)
+        checkers.make_move(move)
+        if checkers.whose_turn() == AI_letter:
+            score = minimax(checkers, 1, AI_letter, depth-1)
         else:
-            score = minimax(board, -1, AI_letter, depth-1)
+            score = minimax(checkers, -1, AI_letter, depth-1)
         scores.append(score)
-        board.undo_move()
-        # board.print()
+        checkers.undo_move()
+
 
     return min(scores) if max_min == -1 else max(scores)
 
 
+
+
 def get_move(board):
     AI = board.whose_turn()
-    best_score, final_move = -10, -1
+    best_score = -1000
+    final_move = []
 
-    possible_moves, order = board.get_possible_moves()
+    possible_moves = board.get_possible_moves()
     for move in possible_moves:
         board.make_move(move)
-        # board.print()
         if board.whose_turn() == AI:
-            score = minimax(board, 1, AI, 14)
+            score = minimax(board, 1, AI, 11)
         else:
-            score = minimax(board, -1, AI, 14)
+            score = minimax(board, -1, AI, 11)
         board.undo_move()
-        # board.print()
-        if score > best_score:
-            best_score, final_move = score, move
 
-    return final_move
+        if score == best_score:
+            final_move.append(move)
+        elif score > best_score:
+            best_score = score
+            final_move = []
+            final_move.append(move)
+        print(move)
 
-def counting(board):
-    a = 0
-    r = 0
-    for row in range(len(board.values)):
-        for kolm in range(len(board.values[row])):
-            if board.values[row][kolm].lower() == 'a':
-                a = a+1
-            elif board.values[row][kolm].lower() == 'r':
-                r = r+1
+    print("best_score: " + str(best_score) + "  final_move len: " + str(len(final_move)))
+    print(final_move)
+    return random.choice(final_move)
 
-    if a > r:
-        return 'a'
-    elif r > a:
-        return 'r'
-    else:
-        return 0
+
+
+
+# def counting(checkers):
+#     a = 0
+#     r = 0
+#     for row in range(len(checkers.board)):
+#         for kolm in range(len(checkers.board[row])):
+#             if checkers.board[row][kolm].lower() == 'a':
+#                 a = a+1
+#             elif checkers.board[row][kolm].lower() == 'r':
+#                 r = r+1
+#
+#     if a > r:
+#         return 'a'
+#     elif r > a:
+#         return 'r'
+#     else:
+#         return 0
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from Board import *
+from Checkers import *
 import random
 import time
 from Minimax import *
 
-# Function that takes a move from the user
-def move_player(board):
-    print('Turn  <' + board.current_player + '>  player')
 
-    possible_moves, order = board.get_possible_moves()
+
+
+
+def move_player(checkers):
+    print('Turn  <' + checkers.current_player + '>  player')
+
+    possible_moves = checkers.get_possible_moves()
     print("possible moves: ")
     print(possible_moves)
 
@@ -16,57 +19,46 @@ def move_player(board):
         print("From which box to which [RowColumn RowColumn]? : ")
         start, end = map(int, sys.stdin.readline().split())
         move = [[start // 10, start % 10], [end // 10, end % 10]]
-        # print(move)
         if move in possible_moves:
-            board.make_move(move)
+            checkers.make_move(move)
             good_move = True
 
 
-# Function that selects and takes a step for the bot
-def move_computer2(board):
-    print('Turn  <' + board.current_player + '>  player')
-    board.make_move(get_move(board))
 
 
-def move_computer(board):
-    print('Turn  <' + board.current_player + '>  player')
+def move_computer2(checkers):
+    print('Turn  <' + checkers.current_player + '>  player')
+    checkers.make_move(get_move(checkers))
+
+
+
+
+def move_computer(checkers):
+    print('Turn  <' + checkers.current_player + '>  player')
     print('AI turn')
 
-    possible_moves, order = board.get_possible_moves()
+    possible_moves = checkers.get_possible_moves()
     print("possible moves: ")
     print(possible_moves)
 
-    board.make_move(random.choice(possible_moves))
+    checkers.make_move(random.choice(possible_moves))
+
 
 
 if __name__ == "__main__":
-
     print('Welcome to English draughts (checkers)!')
-    board = Board()
-    board.print()
+    checkers = Checkers()
+    checkers.print()
 
-    # while board.get_win() is None:
-    #     move_player(board)
-    #     board.print()
-    #
-    # print("Win <" + board.get_win() + ">")
-
-    # x=0
-    while board.get_win() is None:
-        if board.whose_turn() == 'r':
-            move_computer2(board)
-        else:
-            move_computer2(board)
-        board.print()
+    while checkers.get_win() is None:
+        # if checkers.whose_turn() == 'r':
+        #     move_computer2(checkers)
+        # else:
+        #     move_computer2(checkers)
+        move_computer2(checkers)
+        checkers.print()
         time.sleep(0.2)
 
-    print("Win <" + board.get_win() + ">")
+    print("Win <" + checkers.get_win() + ">")
 
 
-    # print(board.get_win())
-    # while board.get_win() is None:
-    #     move_computer(board)
-    #     board.print()
-    #     time.sleep(0.2)
-    #
-    # print("Win <" + board.get_win() + ">")
