17bd4020a5de775e0ecd7d01e5cd472d32ef276a
Ocheretian.Kateryna <226448@edu.p.lodz.pl>
2022-01-26 15:16:48 +0100

    after 2000 cycles

--- GetFromNetwork.py
+++ GetFromNetwork.py
@@ -1,13 +1,19 @@
-from NetworkMove import *
-from NetworkPiece import *
+import NetworkMove
+import NetworkPiece
 import copy
-from main2 import *
+import main2
+import numpy as np
+import math
+from Savery import *
+import random
 
 count_of_bad_moves = 0
+
 
 def get_count_of_bad_moves():
     global count_of_bad_moves
     return count_of_bad_moves
+
 
 def zero_count_of_bad_moves():
     global count_of_bad_moves
@@ -18,22 +24,22 @@ def zero_count_of_bad_moves():
 ile_p_bad = 0
 ile_m_bad = 0
 
+
 def write_to_file_bad_piece(board, piece):
     global ile_p_bad
 
     if ile_p_bad >= 100000:
         data_from_file = np.loadtxt("p_faulty_piece.txt")
         file = open("p_faulty_piece.txt", "w")
-        np.savetxt(file, data_from_file[20000:])
+        np.savetxt(file, data_from_file[30000:])
         file.close()
 
         data_from_file = np.loadtxt("p_faulty_board.txt")
         file = open("p_faulty_board.txt", "w")
-        np.savetxt(file, data_from_file[(32 * 20000):])
+        np.savetxt(file, data_from_file[(32 * 30000):])
         file.close()
 
-        ile_p_bad -= 20000
-
+        ile_p_bad -= 30000
 
     save_board_to_file(board, "p_faulty_board.txt")
     save_smth_to_file(piece, "p_faulty_piece.txt")
@@ -47,21 +53,20 @@ def write_to_file_bad_move(board, piece, move):
 
         data_from_file = np.loadtxt("m_faulty_piece.txt")
         file = open("m_faulty_piece.txt", "w")
-        np.savetxt(file, data_from_file[20000:])
+        np.savetxt(file, data_from_file[30000:])
         file.close()
 
         data_from_file = np.loadtxt("m_faulty_move.txt")
         file = open("m_faulty_move.txt", "w")
-        np.savetxt(file, data_from_file[20000:])
+        np.savetxt(file, data_from_file[30000:])
         file.close()
 
         data_from_file = np.loadtxt("m_faulty_board.txt")
         file = open("m_faulty_board.txt", "w")
-        np.savetxt(file, data_from_file[(32*20000):])
+        np.savetxt(file, data_from_file[(32 * 30000):])
         file.close()
 
-        ile_m_bad -= 20000
-
+        ile_m_bad -= 30000
 
     save_board_to_file(board, "m_faulty_board.txt")
     save_smth_to_file(piece, "m_faulty_piece.txt")
@@ -69,17 +74,10 @@ def write_to_file_bad_move(board, piece, move):
     ile_m_bad += 1
 
 
-
-
-
 def get_rezult_from_network(checkers, model_piece, model_move, is_net_and_net):
     global count_of_bad_moves
-    # checkers_copy = Checkers_state()
 
-    # print("\n---- get_rezult_from_network-----")
     board_2 = copy.deepcopy(checkers.board)
-    # board_2 = [row[:] for row in checkers.board]
-    # checkers.print()
     board_list = []
 
     if checkers.get_current_player() == "a":
@@ -117,22 +115,16 @@ def get_rezult_from_network(checkers, model_piece, model_move, is_net_and_net):
             elif element == 'R':
                 board_list.append(4)
 
-    # print("board_list2", board_list)
     num_list = np.array(board_list)
     train_input = num_list.astype('float32') / 5
-
     train_input = np.reshape(train_input, (1, 32))
 
-    # model_piece = keras.models.load_model("model_piece_3")
     predictions_piece = model_piece.predict(train_input)
     predictions_piece = predictions_piece[0]
-    # print("predictions_piece: ", predictions_piece)
-    # model_piece.save("model_piece_3")
 
     possible_moves = checkers.get_possible_moves()
 
     if checkers.get_current_player() == "a":
-
         for i in range(len(possible_moves)):
             for j in range(len(possible_moves[i])):
                 for k in range(len(possible_moves[i][j])):
@@ -141,22 +133,14 @@ def get_rezult_from_network(checkers, model_piece, model_move, is_net_and_net):
     good_piece = 0
     for iter in range(1, 33):
         piece = np.argmax(predictions_piece)
-        # print("check if piece is ok ", iter, "predictions_piece: ", predictions_piece, "piece: ", piece)
-
-        predictions_piece[piece] = -1
-
+        predictions_piece[piece] = -10
         x1 = math.floor(piece / 4)
         x2 = ((piece % 4) * 2 + 1) if x1 % 2 == 0 else ((piece % 4) * 2)
-
         select_piece = [x1, x2]
         it_found = False
 
-        # print("select_piece ", select_piece)
-
         for possible_move in possible_moves:
-            # print("possible_move ", possible_move)
             if possible_move[0][0] == select_piece[0] and possible_move[0][1] == select_piece[1]:
-                # print("good selected piece: ", select_piece, "\n")
                 it_found = True
                 good_piece = piece
                 break
@@ -167,41 +151,24 @@ def get_rezult_from_network(checkers, model_piece, model_move, is_net_and_net):
             write_to_file_bad_piece(num_list, piece)
             if is_net_and_net == 1:
                 count_of_bad_moves += 1
-            # print(".")
-
-    # x1 = math.floor(good_piece / 4)
-    # x2 = ((good_piece % 4) * 2 + 1) if x1 % 2 == 0 else ((good_piece % 4) * 2)
 
     piece_table = np.zeros((1, 32))
     piece_table[0, good_piece] = 1
-    # print("piece_table: ", piece_table)
     piece_table = piece_table.astype('float32')
-
-    # model_move = keras.models.load_model("model_move_3")
     predictions_move = model_move.predict([piece_table, train_input])
     predictions_move = predictions_move[0]
-    # print("predictions_move: ", predictions_move)
-    # model_move.save("model_move_3")
-
     good_move = 0
 
     for iter in range(1, 33):
-
         move = np.argmax(predictions_move)
-        # print("check if move is ok ", iter, "predictions_move: ", predictions_move, "move: ", move)
-
-        predictions_move[move] = -1
-
+        predictions_move[move] = -10
         y1 = math.floor(move / 4)
         y2 = ((move % 4) * 2 + 1) if y1 % 2 == 0 else ((move % 4) * 2)
-
-        select_move = [y1, y2]
         it_found = False
 
         for possible_move in possible_moves:
-            if possible_move[0][0] == x1 and possible_move[0][1] == x2 and possible_move[1][0] == y1 and \
-                    possible_move[1][1] == y2:
-                # print("good selected move: ", select_move, "\n")
+            if possible_move[0][0] == x1 and possible_move[0][1] == x2 and possible_move[1][
+                0] == y1 and possible_move[1][1] == y2:
                 it_found = True
                 good_move = move
                 break
@@ -212,30 +179,18 @@ def get_rezult_from_network(checkers, model_piece, model_move, is_net_and_net):
             write_to_file_bad_move(num_list, good_piece, move)
             if is_net_and_net == 1:
                 count_of_bad_moves += 1
-            # print(".")
-
-
 
     return num_list, good_piece, good_move;
 
 
-
 def get_rezult_from_rand(checkers):
-
-    # checkers_copy = Checkers_state()
-
-    # print("\n---- get_rezult_from_network-----")
     board_2 = copy.deepcopy(checkers.board)
-    # board_2 = [row[:] for row in checkers.board]
-    # checkers.print()
     board_list = []
 
     if checkers.get_current_player() == "a":
-
         board_2.reverse()
         for i in range(len(board_2)):
             board_2[i].reverse()
-
         for i in range(len(board_2)):
             for j in range(len(board_2[i])):
                 if board_2[i][j] == 'A':
@@ -249,10 +204,8 @@ def get_rezult_from_rand(checkers):
 
     for i in range(len(board_2)):
         for j in range(len(board_2[i])):
-
             if (i + j) % 2 == 0:
                 continue
-
             element = board_2[i][j]
             if element == 'A':
                 board_list.append(0)
@@ -265,29 +218,19 @@ def get_rezult_from_rand(checkers):
             elif element == 'R':
                 board_list.append(4)
 
-    # print("board_list2", board_list)
     num_list = np.array(board_list)
-
 
     possible_moves = checkers.get_possible_moves()
 
     if checkers.get_current_player() == "a":
-
         for i in range(len(possible_moves)):
             for j in range(len(possible_moves[i])):
                 for k in range(len(possible_moves[i][j])):
                     possible_moves[i][j][k] = 7 - possible_moves[i][j][k]
 
-
-    rand_move = possible_moves[random.randint(0, len(possible_moves)-1)]
-    # print("rand_move ", rand_move)
+    rand_move = possible_moves[random.randint(0, len(possible_moves) - 1)]
 
     good_piece = math.floor(rand_move[0][0] * 4 + rand_move[0][1] / 2)
-    # print("good_piece ", good_piece)
     good_move = math.floor(rand_move[1][0] * 4 + rand_move[1][1] / 2)
-    # print("good_move ", good_move)
-
-
-
 
     return num_list, good_piece, good_move;
--- ab_fit_all_models.py
+++ ab_fit_all_models.py
@@ -5,203 +5,186 @@ import random
 
 
 def fit_piece_network_bad_choice():
-    print("\nstart - fit_piece_network_bad_choice")
+    # print("start - fit_piece_network_bad_choice")
 
-    boards = load_board_from_file("p_faulty_board.txt")
-    rand_indexes = random.sample(range(len(boards)), 1024)
-    train_input = []
+    data_boards = load_board_from_file("p_faulty_board.txt")
+    rand_indexes = random.sample(range(len(data_boards)), 4096)
+    input_boards = []
     for index in rand_indexes:
-        train_input.append(boards[index])
-    train_input = np.array(train_input).astype('float32') / 5
+        input_boards.append(data_boards[index])
+    input_boards = np.array(input_boards).astype('float32') / 5
 
-    model = keras.models.load_model("model_piece_3")
-    train_output_piece = model.predict(train_input)
+    model_piece = keras.models.load_model("model_piece_3")
+    output_pieces = model_piece.predict(input_boards)
 
-    pieces = load_smth_from_file("p_faulty_piece.txt").astype('int')
+    data_pieces = load_smth_from_file("p_faulty_piece.txt").astype('int')
     train_pieces = []
     for index in rand_indexes:
-        train_pieces.append(pieces[index])
-    for i in range(1024):
-        train_output_piece[i][train_pieces[i]] = -1
+        train_pieces.append(data_pieces[index])
 
-    history = model.fit(train_input,
-                        y=train_output_piece,
-                        batch_size=32,
-                        epochs=1)
+    for i in range(4096):
+        output_pieces[i][train_pieces[i]] = -1
 
-    model.save("model_piece_3")
-    print("end - fit_piece_network_bad_choice\n")
+    model_piece.fit(input_boards, y = output_pieces, batch_size = 32, epochs = 1, verbose = 0)
 
+    model_piece.save("model_piece_3")
 
 
 def fit_move_network_bad_choice():
-    print("\nstart - fit_move_network_bad_choice")
+    # print("start - fit_move_network_bad_choice")
 
-    boards = load_board_from_file("m_faulty_board.txt")
-    rand_indexes = random.sample(range(len(boards)), 1024)
-    train_input = []
+    data_boards = load_board_from_file("m_faulty_board.txt")
+    rand_indexes = random.sample(range(len(data_boards)), 4096)
+    input_boards = []
     for index in rand_indexes:
-        train_input.append(boards[index])
-    train_input = np.array(train_input).astype('float32') / 5
+        input_boards.append(data_boards[index])
+    input_boards = np.array(input_boards).astype('float32') / 5
 
-    piece_tables = np.zeros((1024, 32))
-    pieces = load_smth_from_file("m_faulty_piece.txt")
-    pieces = pieces.astype('int')
+    input_pieces = np.zeros((4096, 32))
+    data_pieces = load_smth_from_file("m_faulty_piece.txt").astype('int')
     train_pieces = []
     for index in rand_indexes:
-        train_pieces.append(pieces[index])
-    for i in range(len(piece_tables)):
-        piece_tables[i][train_pieces[i]] = 1
+        train_pieces.append(data_pieces[index])
+    for i in range(4096):
+        input_pieces[i][train_pieces[i]] = 1
 
-    model = keras.models.load_model("model_move_3")
-    train_output_move = model.predict([train_input, piece_tables])
+    model_move = keras.models.load_model("model_move_3")
+    output_moves = model_move.predict([input_boards, input_pieces])
 
-    moves = load_smth_from_file("m_faulty_move.txt")
-    moves = moves.astype('int')
+    data_moves = load_smth_from_file("m_faulty_move.txt").astype('int')
     train_moves = []
     for index in rand_indexes:
-        train_moves.append(moves[index])
-    for i in range(len(train_output_move)):
-        train_output_move[i][train_moves[i]] = -1
+        train_moves.append(data_moves[index])
+    for i in range(4096):
+        output_moves[i][train_moves[i]] = -1
 
-    history = model.fit([train_input, piece_tables],
-                        y=train_output_move,
-                        batch_size=32,
-                        epochs=1)
+    model_move.fit([input_boards, input_pieces], y = output_moves, batch_size = 32, epochs = 1,
+                   verbose = 0)
 
-    model.save("model_move_3")
-    print("end - fit_move_network_bad_choice\n")
+    model_move.save("model_move_3")
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 
 def fit_piece_network_good_choice():
-    print("\nstart - fit_piece_network_good_choice")
+    # print("start - fit_piece_network_good_choice")
 
-    boards = load_board_from_file("correct_board_1.txt")
-    rand_indexes = random.sample(range(len(boards)), 128)
-    train_input = []
+    data_boards_1 = load_board_from_file("correct_board_1.txt")
+    rand_indexes = random.sample(range(len(data_boards_1)), 512)
+    boards_1 = []
     for index in rand_indexes:
-        train_input.append(boards[index])
-    train_input = np.array(train_input).astype('float32') / 5
+        boards_1.append(data_boards_1[index])
+    boards_1 = np.array(boards_1).astype('float32') / 5
 
     model = keras.models.load_model("model_piece_3")
-    train_output_piece = model.predict(train_input)
+    predict_piece_1 = model.predict(boards_1)
 
-    pieces = load_smth_from_file("correct_piece_1.txt").astype('int')
-    train_pieces = []
+    data_pieces_1 = load_smth_from_file("correct_piece_1.txt").astype('int')
+    pieces_1 = []
     for index in rand_indexes:
-        train_pieces.append(pieces[index])
+        pieces_1.append(data_pieces_1[index])
 
-    boards_2 = load_board_from_file("correct_board_2.txt")
-    train_input_2 = []
+    data_boards_2 = load_board_from_file("correct_board_2.txt")
+    boards_2 = []
     for index in rand_indexes:
-        train_input_2.append(boards_2[index])
-    train_input_2 = np.array(train_input_2).astype('float32') / 5
+        boards_2.append(data_boards_2[index])
+    boards_2 = np.array(boards_2).astype('float32') / 5
 
-    train_output_piece_2 = model.predict(train_input_2)
+    predict_piece_2 = model.predict(boards_2)
 
-    pieces_2 = load_smth_from_file("correct_move_2.txt").astype('int')
-    train_pieces_2 = []
+    data_move_2 = load_smth_from_file("correct_move_2.txt").astype('int')
+    moves_2 = []
     for index in rand_indexes:
-        train_pieces_2.append(pieces_2[index])
+        moves_2.append(data_move_2[index])
 
-    for i in range(len(rand_indexes)):
-        if train_pieces_2[i] == 100:
-            train_output_piece[i][train_pieces[i]] = 1
-        elif train_pieces_2[i] == -100:
-            train_output_piece[i][train_pieces[i]] = -1
-        elif train_pieces_2[i] == 50:
-            train_output_piece[i][train_pieces[i]] = 0
+    for i in range(512):
+        if moves_2[i] == 100:
+            predict_piece_1[i][pieces_1[i]] = 1
+        elif moves_2[i] == -100:
+            predict_piece_1[i][pieces_1[i]] = -1
+        elif moves_2[i] == 50:
+            predict_piece_1[i][pieces_1[i]] = 0
         else:
-            suma = sum(train_input[i]) - sum(train_input_2[i])
+            suma = sum(boards_1[i]) - sum(boards_2[i])
             ocena = 0
             if suma > 0:
                 ocena = -0.1
             elif suma < 0:
                 ocena = 0.1
-            maxim = max(train_output_piece_2[i])
-            train_output_piece[i][train_pieces[i]] = ocena + 0.8 * maxim
+            maxim = max(predict_piece_2[i])
+            predict_piece_1[i][pieces_1[i]] = ocena + 0.8 * maxim
 
-    history = model.fit(train_input,
-                        y=train_output_piece,
-                        batch_size=32,
-                        epochs=1)
+    model.fit(boards_1, y = predict_piece_1, batch_size = 32, epochs = 1, verbose = 0)
 
     model.save("model_piece_3")
-    print("end - fit_piece_network_good_choice\n")
 
 
 def fit_move_network_good_choice():
-    print("\nstart - fit_piece_network_good_choice")
+    # print("start - fit_piece_network_good_choice")
 
-    boards = load_board_from_file("correct_board_1.txt")
-    rand_indexes = random.sample(range(len(boards)), 128)
-    train_input = []
+    data_boards_1 = load_board_from_file("correct_board_1.txt")
+    rand_indexes = random.sample(range(len(data_boards_1)), 512)
+    boards_1 = []
     for index in rand_indexes:
-        train_input.append(boards[index])
-    train_input = np.array(train_input).astype('float32') / 5
+        boards_1.append(data_boards_1[index])
+    boards_1 = np.array(boards_1).astype('float32') / 5
 
-    piece_tables = np.zeros((128, 32))
-    pieces = load_smth_from_file("correct_piece_1.txt").astype('int')
-    train_pieces = []
+    input_pieces = np.zeros((512, 32))
+    data_pieces_1 = load_smth_from_file("correct_piece_1.txt").astype('int')
+    pieces_1 = []
     for index in rand_indexes:
-        train_pieces.append(pieces[index])
-    for i in range(len(piece_tables)):
-        piece_tables[i][train_pieces[i]] = 1
+        pieces_1.append(data_pieces_1[index])
+    for i in range(512):
+        input_pieces[i][pieces_1[i]] = 1
 
     model = keras.models.load_model("model_move_3")
-    train_output_piece = model.predict(train_input, piece_tables)
+    predict_move_1 = model.predict([boards_1, input_pieces])
 
-    moves = load_smth_from_file("correct_move_1.txt").astype('int')
-    train_moves = []
+    data_moves_1 = load_smth_from_file("correct_move_1.txt").astype('int')
+    moves_1 = []
     for index in rand_indexes:
-        train_moves.append(moves[index])
+        moves_1.append(data_moves_1[index])
 
-    boards_2 = load_board_from_file("correct_board_2.txt")
-    train_input_2 = []
+    data_boards_2 = load_board_from_file("correct_board_2.txt")
+    boards_2 = []
     for index in rand_indexes:
-        train_input_2.append(boards_2[index])
-    train_input_2 = np.array(train_input_2).astype('float32') / 5
+        boards_2.append(data_boards_2[index])
+    boards_2 = np.array(boards_2).astype('float32') / 5
 
-    piece_tables_2 = np.zeros((128, 32))
-    pieces = load_smth_from_file("correct_piece_2.txt")
-    pieces = pieces.astype('int')
-    train_pieces = []
+    piece_tables_2 = np.zeros((512, 32))
+    data_pieces_2 = load_smth_from_file("correct_piece_2.txt").astype('int')
+    pieces_2 = []
     for index in rand_indexes:
-        train_pieces.append(pieces[index])
-    for i in range(len(piece_tables)):
-        piece_tables_2[i][train_pieces[i]] = 1
+        pieces_2.append(data_pieces_2[index])
+    for i in range(512):
+        piece_tables_2[i][pieces_2[i]] = 1
 
-    train_output_piece_2 = model.predict(train_input_2, piece_tables_2)
+    predict_move_2 = model.predict([boards_2, piece_tables_2])
 
-    moves_2 = load_smth_from_file("correct_move_2.txt").astype('int')
-    train_moves_2 = []
+    data_moves_2 = load_smth_from_file("correct_move_2.txt").astype('int')
+    moves_2 = []
     for index in rand_indexes:
-        train_moves_2.append(moves_2[index])
+        moves_2.append(data_moves_2[index])
 
-    for i in range(len(rand_indexes)):
-        if train_moves_2[i] == 100:
-            train_output_piece[i][train_moves[i]] = 1
-        elif train_moves_2[i] == -100:
-            train_output_piece[i][train_moves[i]] = -1
-        elif train_moves_2[i] == 50:
-            train_output_piece[i][train_moves[i]] = 0
+    for i in range(512):
+        if moves_2[i] == 100:
+            predict_move_1[i][moves_1[i]] = 1
+        elif moves_2[i] == -100:
+            predict_move_1[i][moves_1[i]] = -1
+        elif moves_2[i] == 50:
+            predict_move_1[i][moves_1[i]] = 0
         else:
-            suma = sum(train_input[i]) - sum(train_input_2[i])
+            suma = sum(boards_1[i]) - sum(boards_2[i])
             ocena = 0
             if suma > 0:
                 ocena = -0.1
             elif suma < 0:
                 ocena = 0.1
-            maxim = max(train_output_piece_2[i])
-            train_output_piece[i][train_moves[i]] = ocena + 0.8 * maxim
+            maxim = max(predict_move_2[i])
+            predict_move_1[i][moves_1[i]] = ocena + 0.8 * maxim
 
-    history = model.fit([train_input, piece_tables],
-                        y=train_output_piece,
-                        batch_size=32,
-                        epochs=1)
+    model.fit([boards_1, input_pieces], y = predict_move_1, batch_size = 32, epochs = 1,
+              verbose = 0)
 
-    model.save("model_piece_3")
-    print("end - fit_piece_network_good_choice\n")
+    model.save("model_move_3")
