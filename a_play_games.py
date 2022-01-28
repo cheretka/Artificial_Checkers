f3597d009d19e31b97b3c812abe444166bd15ee0
Ocheretian.Kateryna <226448@edu.p.lodz.pl>
2022-01-28 06:47:47 +0100

    reformate code

--- a_play_games.py
+++ a_play_games.py
@@ -64,124 +64,95 @@ def check(network_move, all_moves):
 
 
 def play_game_and_write_moves():
-    # print("   PART 1_2    start   play_game_and_write_moves")
+    checkers = Checkers_state()
+    r_board_1, r_piece_1, r_move_1, a_board_1, a_piece_1, a_move_1 = None, None, None, None, None, None
 
-    model_piece = keras.models.load_model("model_piece_3")
-    model_move = keras.models.load_model("model_move_3")
+    while checkers.get_win() is None:
 
-    for i in range(1):
+        out_board_2, out_piece_2, out_move_2 = get_rezult_from_network(checkers, model_piece,
+                                                                       model_move, 1)
 
-        checkers = Checkers_state()
+        if checkers.get_current_player() == 'r':
+            if r_board_1 is not None:
+                write_to_file(r_board_1, r_piece_1, r_move_1, out_board_2, out_piece_2, out_move_2)
 
-        r_board_1 = None
-        r_piece_1 = None
-        r_move_1 = None
+            r_board_1 = out_board_2
+            r_piece_1 = out_piece_2
+            r_move_1 = out_move_2
 
-        a_board_1 = None
-        a_piece_1 = None
-        a_move_1 = None
-
-        while checkers.get_win() is None:
-
-            if checkers.get_current_player() == 'r':
-
-                r_board_2, r_piece_2, r_move_2 = get_rezult_from_network(checkers, model_piece,
-                                                                         model_move, 1)
-
-                if r_board_1 is not None:
-                    write_to_file(r_board_1, r_piece_1, r_move_1, r_board_2, r_piece_2,
-                                  r_move_2)  # print(r_move_2)
-
-                r_board_1 = r_board_2
-                r_piece_1 = r_piece_2
-                r_move_1 = r_move_2
-
-                x1 = math.floor(r_piece_2 / 4)
-                x2 = ((r_piece_2 % 4) * 2 + 1) if x1 % 2 == 0 else ((r_piece_2 % 4) * 2)
-                y1 = math.floor(r_move_2 / 4)
-                y2 = ((r_move_2 % 4) * 2 + 1) if y1 % 2 == 0 else ((r_move_2 % 4) * 2)
-                selected_move = [[x1, x2], [y1, y2]]
-
-            else:
-
-                a_board_2, a_piece_2, a_move_2 = get_rezult_from_network(checkers, model_piece,
-                                                                         model_move, 1)
-
-                if a_board_1 is not None:
-                    write_to_file(a_board_1, a_piece_1, a_move_1, a_board_2, a_piece_2, a_move_2)
-
-                a_board_1 = a_board_2
-                a_piece_1 = a_piece_2
-                a_move_1 = a_move_2
-
-                x1 = math.floor(a_piece_2 / 4)
-                x2 = ((a_piece_2 % 4) * 2 + 1) if x1 % 2 == 0 else ((a_piece_2 % 4) * 2)
-                y1 = math.floor(a_move_2 / 4)
-                y2 = ((a_move_2 % 4) * 2 + 1) if y1 % 2 == 0 else ((a_move_2 % 4) * 2)
-                # selected_move1 = [[x1, x2], [y1, y2]]
-                selected_move = [[7 - x1, 7 - x2], [7 - y1, 7 - y2]]
-
-                # print("selected_move ", selected_move)
-            selected_move = check(selected_move, checkers.get_possible_moves())
-            if selected_move is None:
-                print("\n-------- the selected move is not possible -> MCTS  ")
-                print("board")
-                print(checkers.current_player)
-                print(checkers.board)
-                # checkers.print()
-                print("selected_move ", selected_move)
-                for i in checkers.get_possible_moves():
-                    print(i)
-                exit()
-
-            checkers = checkers.make_move(selected_move)  # checkers.print(selected_move[0])
-
-        board_2 = [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]
-
-        board_list = []
-
-        for i in range(len(board_2)):
-            for j in range(len(board_2[i])):
-
-                if (i + j) % 2 == 0:
-                    continue
-
-                element = board_2[i][j]
-                if element == 'A':
-                    board_list.append(0)
-                elif element == 'a':
-                    board_list.append(1)
-                elif element == ' ':
-                    board_list.append(2)
-                elif element == 'r':
-                    board_list.append(3)
-                elif element == 'R':
-                    board_list.append(4)
-
-        num_list = np.array(board_list)
-
-        if checkers.get_win() == 'r':
-            write_to_file(r_board_1, r_piece_1, r_move_1, num_list, 1, 100)
-            write_to_file(a_board_1, a_piece_1, a_move_1, num_list, 1, -100)
-
-        elif checkers.get_win() == 'a':
-            write_to_file(r_board_1, r_piece_1, r_move_1, num_list, 1, -100)
-            write_to_file(a_board_1, a_piece_1, a_move_1, num_list, 1, 100)
+            x1 = math.floor(r_piece_1 / 4)
+            x2 = ((r_piece_1 % 4) * 2 + 1) if x1 % 2 == 0 else ((r_piece_1 % 4) * 2)
+            y1 = math.floor(r_move_1 / 4)
+            y2 = ((r_move_1 % 4) * 2 + 1) if y1 % 2 == 0 else ((r_move_1 % 4) * 2)
+            selected_move = [[x1, x2], [y1, y2]]
 
         else:
-            write_to_file(r_board_1, r_piece_1, r_move_1, num_list, 1, 50)
-            write_to_file(a_board_1, a_piece_1, a_move_1, num_list, 1, 50)
+            if a_board_1 is not None:
+                write_to_file(a_board_1, a_piece_1, a_move_1, out_board_2, out_piece_2, out_move_2)
 
-    # print("\n----------------------------------------------------------------------")
+            a_board_1 = out_board_2
+            a_piece_1 = out_piece_2
+            a_move_1 = out_move_2
+
+            x1 = math.floor(a_piece_1 / 4)
+            x2 = ((a_piece_1 % 4) * 2 + 1) if x1 % 2 == 0 else ((a_piece_1 % 4) * 2)
+            y1 = math.floor(a_move_1 / 4)
+            y2 = ((a_move_1 % 4) * 2 + 1) if y1 % 2 == 0 else ((a_move_1 % 4) * 2)
+            selected_move = [[7 - x1, 7 - x2], [7 - y1, 7 - y2]]
+
+        selected_move = check(selected_move, checkers.get_possible_moves())
+        if selected_move is None:
+            print("\n-------- the selected move is not possible -> MCTS  \n", "board\n",
+                  checkers.current_player, checkers.board)
+            checkers.print()
+            print("selected_move ", out_piece_2, "  ", out_move_2)
+            for i in checkers.get_possible_moves():
+                print(i)
+            exit()
+
+        checkers = checkers.make_move(selected_move)
+
+    board_2 = [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+               [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+               [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+               [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]
+
+    board_list = []
+
+    for i in range(len(board_2)):
+        for j in range(len(board_2[i])):
+
+            if (i + j) % 2 == 0:
+                continue
+
+            element = board_2[i][j]
+            if element == 'A':
+                board_list.append(0)
+            elif element == 'a':
+                board_list.append(1)
+            elif element == ' ':
+                board_list.append(2)
+            elif element == 'r':
+                board_list.append(3)
+            elif element == 'R':
+                board_list.append(4)
+
+    num_list = np.array(board_list)
+
+    if checkers.get_win() == 'r':
+        write_to_file(r_board_1, r_piece_1, r_move_1, num_list, 1, 100)
+        write_to_file(a_board_1, a_piece_1, a_move_1, num_list, 1, -100)
+
+    elif checkers.get_win() == 'a':
+        write_to_file(r_board_1, r_piece_1, r_move_1, num_list, 1, -100)
+        write_to_file(a_board_1, a_piece_1, a_move_1, num_list, 1, 100)
+
+    else:
+        write_to_file(r_board_1, r_piece_1, r_move_1, num_list, 1, 50)
+        write_to_file(a_board_1, a_piece_1, a_move_1, num_list, 1, 50)
+
     print(get_count_of_bad_moves())
-    zero_count_of_bad_moves()  # print("zero_count_of_bad_moves ", zero_count_of_bad_moves())  # print("         PART 1     end ")
+    zero_count_of_bad_moves()
 
 
 def play_rand_game_and_write_moves():
@@ -190,124 +161,116 @@ def play_rand_game_and_write_moves():
     model_piece = keras.models.load_model("model_piece_3")
     model_move = keras.models.load_model("model_move_3")
 
-    for i in range(1):
+    checkers = Checkers_state()
+    # checkers.print()
 
-        checkers = Checkers_state()
-        # checkers.print()
+    r_board_1 = None
+    r_piece_1 = None
+    r_move_1 = None
 
-        r_board_1 = None
-        r_piece_1 = None
-        r_move_1 = None
+    a_board_1 = None
+    a_piece_1 = None
+    a_move_1 = None
 
-        a_board_1 = None
-        a_piece_1 = None
-        a_move_1 = None
-
-        while checkers.get_win() is None:
+    while checkers.get_win() is None:
 
 
-            if checkers.get_current_player() == 'r':
+        if checkers.get_current_player() == 'r':
 
-                if random.randint(1, 2) == 1:
-                    r_board_2, r_piece_2, r_move_2 = get_rezult_from_rand(checkers)
-                else:
-                    # r_board_2, r_piece_2, r_move_2 = get_rezult_from_rand(checkers)
-                    r_board_2, r_piece_2, r_move_2 = get_rezult_from_network(checkers, model_piece,
-                                                                             model_move, 0)
-
-                if r_board_1 is not None:
-                    write_to_file(r_board_1, r_piece_1, r_move_1, r_board_2, r_piece_2,
-                                  r_move_2)  # print(r_move_2)
-
-                r_board_1 = r_board_2
-                r_piece_1 = r_piece_2
-                r_move_1 = r_move_2
-
-                x1 = math.floor(r_piece_2 / 4)
-                x2 = ((r_piece_2 % 4) * 2 + 1) if x1 % 2 == 0 else ((r_piece_2 % 4) * 2)
-                y1 = math.floor(r_move_2 / 4)
-                y2 = ((r_move_2 % 4) * 2 + 1) if y1 % 2 == 0 else ((r_move_2 % 4) * 2)
-                selected_move = [[x1, x2], [y1, y2]]
-
+            if random.randint(1, 3) == 1:
+                r_board_2, r_piece_2, r_move_2 = get_rezult_from_rand(checkers)
             else:
+                # r_board_2, r_piece_2, r_move_2 = get_rezult_from_rand(checkers)
+                r_board_2, r_piece_2, r_move_2 = get_rezult_from_network(checkers, model_piece,
+                                                                         model_move, 0)
 
-                if random.randint(1, 2) == 1:
-                    a_board_2, a_piece_2, a_move_2 = get_rezult_from_rand(checkers)
-                else:
-                    a_board_2, a_piece_2, a_move_2 = get_rezult_from_network(checkers, model_piece,
-                                                                             model_move, 0)
+            if r_board_1 is not None:
+                write_to_file(r_board_1, r_piece_1, r_move_1, r_board_2, r_piece_2,
+                              r_move_2)  # print(r_move_2)
 
-                if a_board_1 is not None:
-                    write_to_file(a_board_1, a_piece_1, a_move_1, a_board_2, a_piece_2,
-                                  a_move_2)  # print(a_move_2)
+            r_board_1 = r_board_2
+            r_piece_1 = r_piece_2
+            r_move_1 = r_move_2
 
-                a_board_1 = a_board_2
-                a_piece_1 = a_piece_2
-                a_move_1 = a_move_2
-
-                x1 = math.floor(a_piece_2 / 4)
-                x2 = ((a_piece_2 % 4) * 2 + 1) if x1 % 2 == 0 else ((a_piece_2 % 4) * 2)
-                y1 = math.floor(a_move_2 / 4)
-                y2 = ((a_move_2 % 4) * 2 + 1) if y1 % 2 == 0 else ((a_move_2 % 4) * 2)
-                # selected_move1 = [[x1, x2], [y1, y2]]
-                selected_move = [[7 - x1, 7 - x2], [7 - y1, 7 - y2]]
-
-                # print("selected_move ", selected_move)
-            selected_move = check(selected_move, checkers.get_possible_moves())
-            if selected_move is None:
-                print("\n-------- the selected move is not possible -> MCTS  ")
-                print("board")
-                print(checkers.current_player)
-                print(checkers.board)
-                # checkers.print()
-                print("selected_move ", selected_move)
-                for i in checkers.get_possible_moves():
-                    print(i)
-                exit()
-
-            checkers = checkers.make_move(selected_move)  # checkers.print(selected_move[0])
-
-        board_2 = [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]
-
-        board_list = []
-
-        for i in range(len(board_2)):
-            for j in range(len(board_2[i])):
-
-                if (i + j) % 2 == 0:
-                    continue
-
-                element = board_2[i][j]
-                if element == 'A':
-                    board_list.append(0)
-                elif element == 'a':
-                    board_list.append(1)
-                elif element == ' ':
-                    board_list.append(2)
-                elif element == 'r':
-                    board_list.append(3)
-                elif element == 'R':
-                    board_list.append(4)
-
-        num_list = np.array(board_list)
-
-        if checkers.get_win() == 'r':
-            write_to_file(r_board_1, r_piece_1, r_move_1, num_list, 1, 100)
-            write_to_file(a_board_1, a_piece_1, a_move_1, num_list, 1, -100)
-
-        elif checkers.get_win() == 'a':
-            write_to_file(r_board_1, r_piece_1, r_move_1, num_list, 1, -100)
-            write_to_file(a_board_1, a_piece_1, a_move_1, num_list, 1, 100)
+            x1 = math.floor(r_piece_2 / 4)
+            x2 = ((r_piece_2 % 4) * 2 + 1) if x1 % 2 == 0 else ((r_piece_2 % 4) * 2)
+            y1 = math.floor(r_move_2 / 4)
+            y2 = ((r_move_2 % 4) * 2 + 1) if y1 % 2 == 0 else ((r_move_2 % 4) * 2)
+            selected_move = [[x1, x2], [y1, y2]]
 
         else:
-            write_to_file(r_board_1, r_piece_1, r_move_1, num_list, 1, 50)
-            write_to_file(a_board_1, a_piece_1, a_move_1, num_list, 1, 50)
 
-    # print("\n----------------------------------------------------------------------")  # print("         PART 1_2     end ")
+            if random.randint(1, 3) == 1:
+                a_board_2, a_piece_2, a_move_2 = get_rezult_from_rand(checkers)
+            else:
+                a_board_2, a_piece_2, a_move_2 = get_rezult_from_network(checkers, model_piece,
+                                                                         model_move, 0)
+
+            if a_board_1 is not None:
+                write_to_file(a_board_1, a_piece_1, a_move_1, a_board_2, a_piece_2,
+                              a_move_2)  # print(a_move_2)
+
+            a_board_1 = a_board_2
+            a_piece_1 = a_piece_2
+            a_move_1 = a_move_2
+
+            x1 = math.floor(a_piece_2 / 4)
+            x2 = ((a_piece_2 % 4) * 2 + 1) if x1 % 2 == 0 else ((a_piece_2 % 4) * 2)
+            y1 = math.floor(a_move_2 / 4)
+            y2 = ((a_move_2 % 4) * 2 + 1) if y1 % 2 == 0 else ((a_move_2 % 4) * 2)
+            # selected_move1 = [[x1, x2], [y1, y2]]
+            selected_move = [[7 - x1, 7 - x2], [7 - y1, 7 - y2]]
+
+            # print("selected_move ", selected_move)
+        selected_move = check(selected_move, checkers.get_possible_moves())
+        if selected_move is None:
+            print("\n-------- the selected move is not possible -> MCTS  ")
+            print("board")
+            print(checkers.current_player)
+            print(checkers.board)
+            checkers.print()
+            print("selected_move ", selected_move)
+            for i in checkers.get_possible_moves():
+                print(i)
+            exit()
+
+        checkers = checkers.make_move(selected_move)  # checkers.print(selected_move[0])
+
+    board_2 = [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+               [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+               [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+               [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]
+
+    board_list = []
+
+    for i in range(len(board_2)):
+        for j in range(len(board_2[i])):
+
+            if (i + j) % 2 == 0:
+                continue
+
+            element = board_2[i][j]
+            if element == 'A':
+                board_list.append(0)
+            elif element == 'a':
+                board_list.append(1)
+            elif element == ' ':
+                board_list.append(2)
+            elif element == 'r':
+                board_list.append(3)
+            elif element == 'R':
+                board_list.append(4)
+
+    num_list = np.array(board_list)
+
+    if checkers.get_win() == 'r':
+        write_to_file(r_board_1, r_piece_1, r_move_1, num_list, 1, 100)
+        write_to_file(a_board_1, a_piece_1, a_move_1, num_list, 1, -100)
+
+    elif checkers.get_win() == 'a':
+        write_to_file(r_board_1, r_piece_1, r_move_1, num_list, 1, -100)
+        write_to_file(a_board_1, a_piece_1, a_move_1, num_list, 1, 100)
+
+    else:
+        write_to_file(r_board_1, r_piece_1, r_move_1, num_list, 1, 50)
+        write_to_file(a_board_1, a_piece_1, a_move_1, num_list, 1, 50)
